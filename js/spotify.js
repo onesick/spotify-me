b1ba8e7990eb5bb2ca4d67d7a970f4d5ff417f9e
Wonsik Jeong <wsjeong21@hotmail.com>
2015-11-30 00:37:58 -0500

    cannot link to the correct artist info

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,50 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
+$(document).ready(function(){
 
+$('form#search input[type=submit]').on("click", searchFunc);
+function searchFunc() {
+  event.preventDefault();
+  var search_key = $("#search-keyword").val();
+  var search_type = $("#search-type").val();
+
+  if (search_type==="artist"){
+    searchByArtist(search_key);
+  }
+  else if (search_type==="track") {
+    searchByTrack(search_key);
+  }
+
+
+}
 
 function searchByArtist(keyword) {
   var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done(function(response){
+    $("#results").append("<li><a href="+response.artists.href+"> "+response.artists.name+ "</li>");
+    console.log("ajax loaded");
+  }).fail(function(){
+    console.log("ajax load failed");
+  })
 }
 
 
 function searchByTrack(keyword) {
   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done(function(){
+    console.log("ajax loaded");
+  }).fail(function(){
+    console.log("ajax load failed");
+  })
 }
+
+// end of jQuery load
+})
